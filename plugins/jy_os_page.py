25dd943cb3ba225684ea4ceac00e8cf22fb13824
XiaoYouChR <XiaoYouChR@outlook.com>
2024-08-09 21:31:40 +0800

    更新版本号至 v3.3.0

--- plugins/jy_os_page.py
+++ plugins/jy_os_page.py
@@ -4,19 +4,19 @@ import os
 from pathlib import Path
 
 import httpx
-from PySide6.QtCore import Qt, QThread, Signal, QDir, QUrl
+from PySide6.QtCore import Qt, QThread, Signal, QDir, QUrl, QSize, QMetaObject, QCoreApplication
 from PySide6.QtGui import QPixmap, QColor, QDesktopServices
 from PySide6.QtWidgets import QWidget, QFrame, QVBoxLayout, QSizePolicy, QHBoxLayout, QFileDialog
 from loguru import logger
 from qfluentwidgets import ScrollArea, TitleLabel, isDarkTheme, SettingCardGroup, OptionsConfigItem, OptionsValidator, \
     ComboBoxSettingCard, FluentIcon as FIF, TextEdit, PushSettingCard, RangeSettingCard, RangeConfigItem, \
-    RangeValidator, PrimaryPushButton, PushButton, MessageBox, CardWidget, RoundMenu, Action
+    RangeValidator, PrimaryPushButton, PushButton, MessageBox, CardWidget, RoundMenu, Action, PixmapLabel, BodyLabel, \
+    PrimarySplitPushButton
 from qfluentwidgets.components.dialog_box.mask_dialog_base import MaskDialogBase
 
 from app.common.methods import getWindowsProxy
 from app.common.plugin_base import PluginBase
 from app.common.signal_bus import signalBus
-from app.components.Ui_SystemInfoCard import Ui_SystemInfoCard
 
 
 class JyOSPagePlugin(PluginBase):
@@ -264,6 +264,73 @@ class DownloadOptionDialog(MaskDialogBase):
 
     def _onCurrentIndexChanged(self, Index: int):
         self.logTextEdit.setText(self.list[Index]["Log"])
+
+
+class Ui_SystemInfoCard(object):
+    def setupUi(self, SystemInfoCard):
+        if not SystemInfoCard.objectName():
+            SystemInfoCard.setObjectName(u"SystemInfoCard")
+        SystemInfoCard.resize(793, 131)
+        SystemInfoCard.setMinimumSize(QSize(793, 131))
+        SystemInfoCard.setMaximumSize(QSize(16777215, 131))
+        self.horizontalLayout = QHBoxLayout(SystemInfoCard)
+        self.horizontalLayout.setSpacing(12)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.LogoPixmapLabel = PixmapLabel(SystemInfoCard)
+        self.LogoPixmapLabel.setObjectName(u"LogoPixmapLabel")
+        self.LogoPixmapLabel.setMinimumSize(QSize(101, 101))
+        self.LogoPixmapLabel.setMaximumSize(QSize(101, 101))
+        self.LogoPixmapLabel.setScaledContents(True)
+        self.LogoPixmapLabel.setAlignment(Qt.AlignCenter)
+
+        self.horizontalLayout.addWidget(self.LogoPixmapLabel)
+
+        self.BodyVBoxLayout = QVBoxLayout()
+        self.BodyVBoxLayout.setSpacing(0)
+        self.BodyVBoxLayout.setObjectName(u"BodyVBoxLayout")
+        self.TitleLabel = TitleLabel(SystemInfoCard)
+        self.TitleLabel.setObjectName(u"TitleLabel")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.TitleLabel.sizePolicy().hasHeightForWidth())
+        self.TitleLabel.setSizePolicy(sizePolicy)
+
+        self.BodyVBoxLayout.addWidget(self.TitleLabel)
+
+        self.BodyLabel = BodyLabel(SystemInfoCard)
+        self.BodyLabel.setObjectName(u"BodyLabel")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.BodyLabel.sizePolicy().hasHeightForWidth())
+        self.BodyLabel.setSizePolicy(sizePolicy1)
+        self.BodyLabel.setMaximumSize(QSize(16777215, 101))
+        self.BodyLabel.setWordWrap(True)
+
+        self.BodyVBoxLayout.addWidget(self.BodyLabel)
+
+        self.horizontalLayout.addLayout(self.BodyVBoxLayout)
+
+        self.PrimarySplitPushButton = PrimarySplitPushButton(SystemInfoCard)
+        self.PrimarySplitPushButton.setObjectName(u"PrimarySplitPushButton")
+        self.PrimarySplitPushButton.setMinimumSize(QSize(121, 31))
+        self.PrimarySplitPushButton.setMaximumSize(QSize(121, 31))
+
+        self.horizontalLayout.addWidget(self.PrimarySplitPushButton)
+
+        self.retranslateUi(SystemInfoCard)
+
+        QMetaObject.connectSlotsByName(SystemInfoCard)
+
+    # setupUi
+
+    def retranslateUi(self, SystemInfoCard):
+        SystemInfoCard.setWindowTitle(QCoreApplication.translate("SystemInfoCard", u"Form", None))
+        self.PrimarySplitPushButton.setProperty("text_", QCoreApplication.translate("SystemInfoCard",
+                                                                                    u"       \u4e0b\u8f7d       ",
+                                                                                    None))
+    # retranslateUi
 
 
 class SystemInfoCard(CardWidget, Ui_SystemInfoCard):
