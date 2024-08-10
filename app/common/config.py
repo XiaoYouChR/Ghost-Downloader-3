5a467f43afac2a3daf8785b050799f970a588561
XiaoYouChR <XiaoYouChR@outlook.com>
2024-08-10 13:47:58 +0800

    更新版本号至3.3.1

--- app/common/config.py
+++ app/common/config.py
@@ -29,7 +29,7 @@ class Config(QConfig):
 
 YEAR = 2024
 AUTHOR = "XiaoYouChR"
-VERSION = "3.3.0"
+VERSION = "3.3.1"
 AUTHOR_URL = "https://space.bilibili.com/437313511"
 FEEDBACK_URL = "https://github.com/XiaoYouChR/Ghost-Downloader-3/issues"
 # RELEASE_URL = "https://github.com/XiaoYouChR/Ghost-Downloader-3/releases/latest"
