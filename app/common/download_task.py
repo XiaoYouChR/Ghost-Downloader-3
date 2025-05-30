01dc21ba69924b0b67477846063bc0c65eca7938
AlphaQian <154665575+Alpha-Qian@users.noreply.github.com>
2025-05-30 19:50:55 +0800

    cache

--- app/common/download_task.py
+++ app/common/download_task.py
@@ -715,3 +715,35 @@ class DownloadTask(QThread):
             self.loop.close()
         except Exception as e:
             logger.error(f"Error cleaning up event loop: {e}")
+
+
+
+
+class SpeedRecoder:
+    def __init__(self,process = 0):
+        self.process = process
+        self.start_time = time.time()
+
+    def reset(self, process):
+        self.process = process
+        self.start_time = time.time()
+
+    def flash(self, process):
+        
+        d_time = time.time() - self.start_time
+        #if d_time != 0:
+        speed = (process - self.process) / (d_time)
+        #else:
+        #    logger.warning("Time cannot be zero")
+        #    speed = 0
+        #    d_time = 0.01#天天出花里胡哨的bug烦死我了
+        return SpeedInfo(speed, d_time)
+
+
+class SpeedInfo:
+    def __init__(self, speed = 0, time = 1):
+        if time != 0:
+            self.speed = speed
+            self.time = time
+        else:
+            raise ValueError("Time cannot be zero")
