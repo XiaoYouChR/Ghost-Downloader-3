efb0e7abdf301ce0430858131a02dc8273a0af2d
XiaoYouChR <xiaoyouchr@outlook.com>
2026-05-30 00:27:27 +0800

    chore(config): 更新版本号到 3.10.1
    - 将 VERSION 从 3.10 修改为 3.10.1
    - 保持其他配置信息不变

--- app/supports/config.py
+++ app/supports/config.py
@@ -358,7 +358,7 @@ class Config(QConfig):
 
 YEAR = 2026
 AUTHOR = "XiaoYouChR"
-VERSION = "3.10"
+VERSION = "3.10.1"
 LATEST_EXTENSION_VERSION = "1.3.0"
 AUTHOR_URL = "https://space.bilibili.com/437313511"
 FEEDBACK_URL = "https://github.com/XiaoYouChR/Ghost-Downloader-3/issues"
