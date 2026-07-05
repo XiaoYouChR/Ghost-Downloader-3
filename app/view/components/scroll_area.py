35d6e2f362df326c27ec4fafd3cc649277127987
XiaoYouChR <xiaoyouchr@outlook.com>
2026-07-05 15:45:23 +0800

    fix: 修复 macOS 滚动条样式异常

--- app/view/components/scroll_area.py
+++ app/view/components/scroll_area.py
@@ -1,12 +1,7 @@
 import sys
 
 if sys.platform == "darwin":
-    from PySide6.QtWidgets import QScrollArea
-
-    class ScrollArea(QScrollArea):
-        def enableTransparentBackground(self):
-            self.setStyleSheet("QScrollArea{background:transparent;border:none}")
-            self.viewport().setStyleSheet("background:transparent")
+    from qfluentwidgets import ScrollArea
 else:
     from qfluentwidgets import SmoothScrollArea
 
