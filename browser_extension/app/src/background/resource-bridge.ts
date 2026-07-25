e68afea4189aafffcbc6143df67b131fc188576f
XiaoYouChR <xiaoyouchr@outlook.com>
2026-07-25 14:35:30 +0800

    Revert "fix(Browser Extension): 拦截下载时发送原始 URL 而非重定向后的 CDN URL"
    This reverts commit 76c97ef6cc4294c772417b25fef8ad545e6358c7.
    
    fix #637, fix #638, fix #617, fix #614, fix #611

--- browser_extension/app/src/background/resource-bridge.ts
+++ browser_extension/app/src/background/resource-bridge.ts
@@ -431,7 +431,7 @@ export function createResourceBridge(options: {
         source: "download",
         title: filename,
         payload: {
-          url: downloadItem.url,
+          url: finalUrl,
           headers,
           filename: filename,
           size:
