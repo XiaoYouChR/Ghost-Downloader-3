563e14dd89e8e75cb5dfa25eb58902ed92c2252b
i0ntempest <i0ntempest@i0ntempest.com>
2026-08-04 21:29:16 +1000

    feat: code changes for i18n

---
+++ features/yt_dlp_pack/label_catalog.py
@@ -0,0 +1,7 @@
+from PySide6.QtCore import QT_TRANSLATE_NOOP as N
+
+N("YtDlpTaskCard", "提取信息")
+N("YtDlpTaskCard", "下载视频")
+N("YtDlpTaskCard", "下载音频")
+N("YtDlpTaskCard", "合并")
+N("YtDlpTaskCard", "下载字幕")
