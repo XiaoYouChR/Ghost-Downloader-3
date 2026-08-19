2d3415a7ff16d0335f4b096d8b3c9a2cf8aa00dd
XiaoYouChR <xiaoyouchr@outlook.com>
2026-08-19 15:36:12 +0800

    chore: bump version to 4.3.3

--- app/config/constants.py
+++ app/config/constants.py
@@ -1,6 +1,6 @@
 YEAR = 2026
 AUTHOR = "XiaoYouChR"
-VERSION = "4.3.2"
+VERSION = "4.3.3"
 BUILD_VERSION = VERSION.split("-")[0]
 DESKTOP_ID = "io.github.xiaoyouchr.GhostDownloader"
 DESKTOP_OBJECT_PATH = "/" + DESKTOP_ID.replace(".", "/")
